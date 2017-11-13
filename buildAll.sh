9b92060b9ab50a5832797217941b7eae87f543fb
Jens Hoepken <jens@sourceflux.de>
2017-11-13 12:50:42 +0100

    Add build script

---
+++ buildAll.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Providing fallback for multicore compilation
+NCORES=$1
+if ! [[ $NCORES =~ ^-?[0-9]+$ ]]
+then
+    NCORES=$(cat /proc/cpuinfo | grep processor | tail -1 | awk '{print $3+1 }')
+fi
+
+(cd ducts && qmake && make -j $NCORES)
+(cd ftrExport && qmake && make -j $NCORES)
